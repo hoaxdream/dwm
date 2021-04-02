1eebfd9707639eb2e7527322adc60ca2012f2e7e
hoaxdream <hoaxdream@gmail.com>
2021-04-03 06:05:06 +0800

    Minor edit

--- config.def.h
+++ config.def.h
@@ -85,7 +85,7 @@ static Key keys[] = {
   /* modifier                     key        function        argument */
   { MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
   { MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
-  { MODKEY,                       XK_b,      togglebar,      {0} },
+  { ALTKEY,                       XK_b,      togglebar,      {0} },
   { MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
   { MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
   { ALTKEY,                       XK_comma,  incnmaster,     {.i = +1 } },
