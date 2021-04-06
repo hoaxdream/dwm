5c40656c43f5e65ca7d3ec88002a56ba68a840a3
hoaxdream <hoaxdream@gmail.com>
2021-04-07 03:02:21 +0800

    Minor edit to tags

--- config.def.h
+++ config.def.h
@@ -22,7 +22,8 @@ static const char *colors[][3]      = {
 
 /* tagging */
 /*static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" }; */
-static const char *tags[] = { "1:home", "2:web", "3:chat", "4:games", "5:dev" };
+/*static const char *tags[] = { "1:home", "2:web", "3:chat", "4:games", "5:dev" }; */
+static const char *tags[] = { "1:", "2:", "3:", "4:", "5:" };
 
 static const Rule rules[] = {
   /* xprop(1):
