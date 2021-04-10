ebc9b5bac5eb2a7a866dfeda52550d360471e6cf
hoaxdream <hoaxdream@gmail.com>
2021-04-10 16:39:14 +0800

    Minor edit

--- config.def.h
+++ config.def.h
@@ -6,7 +6,7 @@ static const int gappx              = 15;        /* gaps between windows */
 static const unsigned int snap      = 0;        /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=16", "JoyPixels:size=16:antialias=true:autohint=true", "Symbols Nerd Font:size=20:style=bold:antialias=true:autohint=true" };
+static const char *fonts[]          = { "monospace:size=16", "JoyPixels:size=16:antialias=true:autohint=true", "Symbols Nerd Font:size=18:style=bold:antialias=true:autohint=true" };
 static const char dmenufont[]       = "monospace:size=16";
 static const char normbgcolor[]     = "#222222"; /* main bar color */
 static const char normbordercolor[] = "#444444"; /* border color unfocused windows */
